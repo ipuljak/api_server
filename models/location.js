fd71661c079fa2586bc233c9ade1c8a1a98d622b
Ivan <ivanpulj@gmail.com>
2016-11-29 01:20:39 -0500

    Updated GET and POST API calls for locations

--- models/location.js
+++ models/location.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
 
+// Define the schema for the location of our street view component
 var locationSchema = new Schema({
-    //id: Schema.Types.ObjectId,
     type: String,
     name: String,
     location: {
@@ -12,13 +13,17 @@ var locationSchema = new Schema({
     },
     position: {
         lat: Number,
-        lng: Number
+        lng: Number,
+        heading: Number,
+        pitch: Number,
+        zoom: Number
     },
-    zoom: Number,
-    questions: [String],
-    plays: Number,
-    averageTime: Number,
-    userCreated: Boolean
+    stats: {
+        plays: Number,
+        averageTime: Number,
+        userCreated: Boolean
+    },
+    questions: [String]
 });
 
 module.exports = mongoose.model("Location", locationSchema);
