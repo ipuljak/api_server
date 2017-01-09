e48bc61f84982bd879af488efb47696f9bab7483
Ivan <ivanpulj@gmail.com>
2017-01-09 01:08:41 -0500

    Refactoring API paths

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-var express        = require("express"),
-	bodyparser     = require("body-parser"),
-    morgan         = require("morgan"),
-	mongoose       = require("mongoose"),
-    cors           = require("cors"),
-	Location       = require("./models/location"),
-	User           = require("./models/user"),
+var express        = require('express'),
+	bodyparser     = require('body-parser'),
+    morgan         = require('morgan'),
+	mongoose       = require('mongoose'),
+    cors           = require('cors'),
+	Location       = require('./models/location'),
+	User           = require('./models/user'),
+	config         = require('./config'),
 	app            = express();
 
 // Routes setup
-var streetViewAPI  = require("./routes/street_view"),
-    authRoutes = require("./routes/auth");
+var streetViewAuth = require('./routes/street_view/auth'),
+	streetViewCategories = require('./routes/street_view/categories'),
+	streetViewComments = require('./routes/street_view/comments'),
+	streetViewCountries = require('./routes/street_view/countries'),
+	streetViewInfo = require('./routes/street_view/info'),
+	streetViewLocations = require('./routes/street_view/locations');
 
 // Database setup
-mongoose.connect('mongodb://localhost/street_view');
+var databaseLink = 'mongodb://default:' + config.password + '@localhost/street_view';
+mongoose.connect(databaseLink);
 
 // App Setup
 app.use(morgan('combined'));
@@ -22,12 +28,17 @@ app.use(bodyparser.json({type: '*/*'}));
 // Define the routes for our app to use
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
-})
+});
 
-app.use("/auth", authRoutes);
-app.use("/api/street_view", streetViewAPI);
+app.use('/api/street_view/auth', streetViewAuth);
+app.use('/api/street_view/categories', streetViewCategories);
+app.use('/api/street_view/comments', streetViewComments);
+app.use('/api/street_view/countries', streetViewCountries);
+app.use('/api/street_view/info', streetViewInfo);
+app.use('/api/street_view/locations', streetViewLocations);
+
 
 // Start the server and listen on the specified port
 app.listen(3001, process.env.IP, function() {
-	console.log("The StreetView API server has started on port 3001.");
+	console.log('The API server has started on port 3001.');
 }); 
---
+++ routes/street_view/info.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// Load in the models
+const Location = require('../../models/location');
+const Country = require('../../models/country');
+const Category = require('../../models/category');
+
+/**
+ *  GET distinct locations API call. To use:
+ *      -> http://localhost:3001/api/street_view/get_distincts
+ *      It returns a list of all the distinct types and cities.
+ */
+router.get('/get_distincts', function(req, res) {
+    var distincts = {};
+
+    Category.find({}, function(err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            distincts['type'] = result.sort(function(a,b) {
+                return a.name.localeCompare(b.name);
+            });
+        }
+    });
+
+    Country.find({}, {'name':1, 'data.image':1}, function(err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            distincts['country'] = result.sort(function(a,b) {
+                return a.name.localeCompare(b.name);
+            });
+            res.send(distincts);
+        }
+    });
+});
+
+/**
+ *  GET location API call. To use:
+ *      -> http://localhost:3001/api/street_view/search_locations?search=[term]
+ *      where search can be any city or location type.
+ *      Returns all resulting locations.
+ */
+router.get('/search_locations', function(req, res) {
+
+    var term = {};
+
+    // Search either by location type, landmark, or city
+    if (req.query.search === 'landmark') {
+        term = {'landmark':true};
+    } else {
+        term = {$or:[{'type':req.query.search}, 
+            {'location.city':req.query.search}]};
+    }
+
+    Location.find(term, function(err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+/**
+ *  GET country info API call. To use:
+ *      -> http://localhost:3001/api/street_view/get_country_info?country=[country]
+ *      It returns the information of a given country along with it's cities.
+ */
+router.get('/get_country_info', function(req, res) {
+    var info = {};
+    var country = req.query.country;
+    var term = {'location.country': country};
+
+    Country.findOne({name: country}, function(err, result) {
+        if (err) {
+            res.send(404);
+        } else {
+            info['country'] = result;
+
+            Location.distinct('location.city', term, function(err, result) {
+                if (err) {
+                    res.send(404);
+                } else {
+                    info['cities'] = result;
+                    res.send(info);
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
